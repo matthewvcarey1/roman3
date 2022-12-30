7f5638b5f2a06bcf277ec814f88f7a6ec315157d
MatthewCarey <mcarey@kidsloop.live>
2022-12-30 08:52:39 +0000

    refacter a name

--- src/main/java/uk/co/example/IntToRomanConverter.java
+++ src/main/java/uk/co/example/IntToRomanConverter.java
@@ -10,19 +10,19 @@ import java.lang.Math;
 
 public class IntToRomanConverter {
     private final long BASE = 10;
-    private static final String message = "Expected as an input parameter of a valid positive integer greater than 0, and less than ";
+    private static final String ARGUMENT_ERROR_MESSAGE = "Expected as an input parameter of a valid positive integer greater than 0, and less than ";
     public static void main(String[] args) {
         IntToRomanConverter converter = new IntToRomanConverter();
         long limit = converter.getTopLimit();
         try {
             final long value = (args.length > 0) ? Long.parseLong(args[0]) : 0;
             if(!converter.validate(value)){
-                System.out.println(message + limit);
+                System.out.println(ARGUMENT_ERROR_MESSAGE + limit);
                 return;
             }
             System.out.println(converter.convert(value));
         } catch (Exception e){
-            System.out.println(message + limit);
+            System.out.println(ARGUMENT_ERROR_MESSAGE + limit);
         }
     }
     private ArrayList<RomanPowerOfTen> romanPowers;
@@ -39,7 +39,7 @@ public class IntToRomanConverter {
             while (romanNumbersIterator.hasNext()) {
                 Object ob = romanNumbersIterator.next();
                 JSONObject romanObject = (JSONObject)ob;
-                long unit = (Long) romanObject.get("unit");
+                final long unit = (Long) romanObject.get("unit");
                 this.limit =  unit * BASE;
                 JSONArray romans = (JSONArray) romanObject.get("romans");
                 ArrayList<String> romansList = new ArrayList<>();
@@ -63,16 +63,16 @@ public class IntToRomanConverter {
         }
     }
     public String convert(long decimal){
-        // I should really check if Math.log!0() is actually quicker than the n iterations in avoids
+        // I should really check if Math.log!0() is actually quicker than the n empty iterations in avoids
         final int startIndex = (int) Math.log10(decimal);
         StringBuilder result = new StringBuilder();
         ListIterator<RomanPowerOfTen> iterator = romanPowers.listIterator((int) startIndex < romanPowers.size()? startIndex+1 : romanPowers.size());
         while(iterator.hasPrevious()){
             RomanPowerOfTen rpt = iterator.previous();
             final long unit = rpt.getUnit();
-            final long lastUnit = unit * BASE;
+            final long previousUnit = unit * BASE;
             // indexOfString: always a value in the range 0-9
-            final int indexOfString = (int) ((decimal % lastUnit) / unit);
+            final int indexOfString = (int) ((decimal % previousUnit) / unit);
             result.append(rpt.getRoman(indexOfString));
         }
         return result.toString();
